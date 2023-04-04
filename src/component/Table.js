31d998c5bc7ea63d8fcf88a176db9189e878473b
biswajit <biswajit@tradelab.in>
2023-04-04 16:52:05 +0530

    search implication

--- src/component/Table.js
+++ src/component/Table.js
@@ -1,5 +1,6 @@
-import React, {memo} from "react"
+import React, {memo, useEffect, useState} from "react"
 import {
+    Input,
 
     Table,
     TableBody,
@@ -9,28 +10,46 @@ import {
     TableRow
 } from "@mui/material";
 const TableChart = ({tableData}) => {
+    const [newtableData, setNewtableData]=useState([])
+    const [input, setInput] = useState("")
+
+    useEffect(()=> {
+        let date = [...tableData]
+        date = date.sort((a, b)=> a.publisherId < b.publisherId ? -1 : 1)
+        if(input){
+            setNewtableData(date.filter(d=>(d.publisherId.toLowerCase().includes(input))))
+        }
+        else{
+            setNewtableData(date)
+        }
+    }, [input])
+
     return (
-        <TableContainer>
-            <Table size="small" aria-label="a dense table">
-                <TableHead>
-                    <TableRow>
-                        <TableCell>Publisher ID</TableCell>
-                        <TableCell>Impression Order</TableCell>
-                    </TableRow>
-                </TableHead>
-                <TableBody>
-                    {tableData.map((row) => (
-                        <TableRow
-                            key={row.name}
-                            sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
-                        >
-                            <TableCell>{row.publisherId}</TableCell>
-                            <TableCell>{row.impressions_offered}</TableCell>
+        <>
+            <Input value={input} onChange={(e)=> setInput(e.target.value)}/>
+            <TableContainer>
+                <Table size="small" aria-label="a dense table">
+                    <TableHead>
+                        <TableRow>
+                            <TableCell>Publisher ID</TableCell>
+                            <TableCell>Impression Order</TableCell>
                         </TableRow>
-                    ))}
-                </TableBody>
-            </Table>
-        </TableContainer>
+                    </TableHead>
+                    <TableBody>
+                        {newtableData.map((row) => (
+                            <TableRow
+                                key={row.name}
+                                sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+                            >
+                                <TableCell>{row.publisherId}</TableCell>
+                                <TableCell>{row.impressions_offered}</TableCell>
+                            </TableRow>
+                        ))}
+                    </TableBody>
+                </Table>
+            </TableContainer>
+
+        </>
     )
 
 }
